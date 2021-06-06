a7cb0ebd503a1280dcd8d48a93676d6e71a44a6d
Whermerson19 <whermersonc@gmail.com>
2021-06-06 16:22:14 -0300

    finish auth context

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,37 @@
-import React, { useContext, createContext, useState } from "react";
+import React, {
+  useContext,
+  createContext,
+  useState,
+  useEffect,
+  useCallback,
+} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import api from "../services/api";
 
-interface AuthContextData {}
+interface SignInCredentials {
+  loginField: string;
+  password: string;
+}
+
+interface User {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  avatarURL: string;
+}
+
+interface AuthContextData {
+  token: string;
+  user: User;
+  signIn(data: SignInCredentials): Promise<void>;
+  signOut(): Promise<void>;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
 
 interface AuthProviderProps {
   children: React.ReactNode;
@@ -17,7 +48,59 @@ export function useAuth(): AuthContextData {
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [data, setDate] = useState(() => {});
+  const [data, setData] = useState<AuthState>({} as AuthState);
 
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const loadData = useCallback(async () => {
+    const [token, user] = await AsyncStorage.multiGet([
+      "@MemoRise:token",
+      "@MemoRise:user",
+    ]);
+
+    if (token[1] && user[1]) {
+      api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
+      setData({
+        token: token[1],
+        user: JSON.parse(user[1]),
+      });
+    }
+  }, []);
+
+  const signIn = useCallback(async ({ loginField, password }) => {
+    const response = await api.post("/session", {
+      loginField,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    await AsyncStorage.multiSet([
+      ["@MemoRise:token", token],
+      ["@MemoRise:user", JSON.stringify(user)],
+    ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
+    setData({
+      token,
+      user,
+    });
+  }, []);
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(["@MemoRise:token", "@MemoRise:user"]);
+
+    setData({} as AuthState);
+  }, []);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ token: data.token, user: data.user, signIn, signOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
